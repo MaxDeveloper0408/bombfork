485e20044186b9100be64e068bdda76e15a12dd1
berryfactory <102176844+bonfirecro@users.noreply.github.com>
2022-05-05 11:50:28 +0530

    init

---
+++ src/features/configure/vault/cronos_pools.js
@@ -0,0 +1,29 @@
+export const cronosPools = [
+    {
+        id: 'bfire-bfire-wcro',
+        name: 'BFIRE-WCRO LP',
+        token: 'BFIRE-WCRO LP',
+        tokenDescription: 'bfire.money',
+        tokenAddress: '0x60337372A65A709dE0BBCB4A27Caa3e912f8875d',
+        tokenDecimals: 18,
+        tokenDescriptionUrl: '#',
+        earnedToken: 'bfire farmBFIRE-WCRO',
+        earnedTokenAddress: '0x41c6e2D30B44e3D89f4C5aeb273286e8907578e2',
+        earnContractAddress: '0x41c6e2D30B44e3D89f4C5aeb273286e8907578e2',
+        pricePerFullShare: 1,
+        tvl: 0,
+        oracle: 'lps',
+        oracleId: 'bfire-bfire-wcro',
+        oraclePrice: 0,
+        depositsPaused: false,
+        status: 'active',
+        platform: 'PancakeSwap',
+        assets: ['BFIRE', 'WCRO'],
+        risks: ['COMPLEXITY_LOW', 'BATTLE_TESTED', 'IL_LOW', 'AUDIT', 'CONTRACTS_VERIFIED'],
+        stratType: 'StratLP',
+        addLiquidityUrl:
+          'https://pancakeswap.finance/add/0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c/0x522348779DCb2911539e76A1042aA922F9C47Ee3',
+        buyTokenUrl:
+          'https://pancakeswap.finance/swap?inputCurrency=0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c&outputCurrency=0x522348779DCb2911539e76A1042aA922F9C47Ee3',
+      },
+];
